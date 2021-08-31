70ae9029aa8d1fa9751f3ebe78af4f7ea50a075d
Nelkit Chavez <nelkitisael792@gmail.com>
2021-08-30 18:20:39 -0600

    Initial Commit

---
+++ TribalTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TribalTestApp
+//
+//  Created by Nelkit Chavez on 30/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
